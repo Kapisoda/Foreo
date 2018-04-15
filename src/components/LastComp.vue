82de6b4d60144aae71ae3495e7d367ef379ddcb5
Filip Kapusta <filipkapusta1@gmail.com>
2018-04-15 22:07:02 +0200

    changed last component

--- src/components/LastComp.vue
+++ src/components/LastComp.vue
@@ -2,7 +2,8 @@
   <div class="last-section">
       <div class="row">
           <hr>
-          <p>testing</p>
+          <img style="float: right; padding-top: 10px;" src='../assets/img/logo_FOREO.svg' alt="Foreo logo">
+            <p class="small-text">&copy; 2018 FILIP KAPUSTA ALL RIGHTS RESERVED</p>
       </div>
   </div>
 </template>
@@ -12,8 +13,12 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
-
+<style lang="scss" scoped>
+    .small-text{
+        font-size: 60%;
+        padding-top: 15px;
+        font-weight: 400;
+    }
 </style>
 
 
